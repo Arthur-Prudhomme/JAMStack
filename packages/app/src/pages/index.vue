5c25aa4fa783b6a767f82edffe779f72a657cf79
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2024-01-29 14:28:20 +0100

    added fetch all and single item

--- packages/app/src/pages/index.vue
+++ packages/app/src/pages/index.vue
@@ -1,18 +1,35 @@
 <script lang="ts" setup>
+const { find } = useStrapi();
+const {
+	data: recettes,
+	pending,
+	error,
+} = useAsyncData("recettes", () =>
+	find("recettes", {
+		populate: "*",
+	})
+);
+console.log(recettes);
 </script>
 
 <template>
-  <div class="container">
-    <div class="flex flex-col items-center gap-y-4">
-      <h1>Démarrage Nuxt Strapi</h1>
-      <div class="flex gap-x-4">
-        <NuxtLink to="/start">
-          Documentation
-        </NuxtLink>
-        <NuxtLink to="/exemple-recherche">
-          Exemple de recherche
-        </NuxtLink>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div class="flex flex-col items-center gap-y-4">
+			<h1>ça va ça avance</h1>
+			<div class="flex gap-x-4">
+				<NuxtLink to="/start"> Documentation </NuxtLink>
+				<NuxtLink to="/exemple-recherche"> Exemple de recherche </NuxtLink>
+			</div>
+		</div>
+
+		<div>
+			<ul>
+				<li v-for="recette in recettes.data" :key="recette.id">
+					<NuxtLink :to="{ name: 'infos', query: { id: recette.id } }">{{
+						recette.title
+					}}</NuxtLink>
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
---
+++ packages/app/src/pages/infos.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const { find } = useStrapi();
+const route = useRoute();
+
+const {
+	data: recettes,
+	pending,
+	error,
+} = useAsyncData(`recettes/${route.params.id}`, () =>
+	find("recettes", {
+		populate: "*",
+	})
+);
+console.log(recettes);
+</script>
+
+<template>
+	<div>hello</div>
+	<pre>
+        {{ recettes }}
+    </pre
+	>
+</template>
